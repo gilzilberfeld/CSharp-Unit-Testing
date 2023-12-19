6284c92a01e551b190406369090a8619c7201a95
gilzilberfeld <gil.zilberfeld@gmail.com>
2023-12-19 12:11:41 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTestingCourse
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+}
